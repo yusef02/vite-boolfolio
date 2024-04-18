ee8e60f723921d27030a531e773cf08176b12c32
yusef02 <youssef.bouziane2402@gmail.com>
2024-04-18 19:46:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script></script>
+<template>
+	<h1>Vite boolfolio</h1>
+</template>
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/css/general.scss";
+
+createApp(App).mount("#app");
